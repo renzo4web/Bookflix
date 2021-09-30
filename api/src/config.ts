6c6e3ab1d8856e36bd6cedee626fa5afe702b9d1
Renzo <w3renzo@gmail.com>
2021-09-30 16:16:38 -0300

    Fixed update book, return with data from first save

--- api/src/config.ts
+++ api/src/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export default {
-    jwtSecret: process.env.JWT_SECRET || "thisisasecret",
-    PORT: process.env.PORT || 5000,
-    CNN_STRING: process.env.CNN_STRING || "http://localhost:5000",
+  jwtSecret: process.env.JWT_SECRET || "thisisasecret",
+  PORT: process.env.PORT || 5000,
+  CNN_STRING: process.env.CNN_STRING || "http://localhost",
 };
--- api/src/controllers/books.controllers.ts
+++ api/src/controllers/books.controllers.ts
@@ -11,82 +11,85 @@ interface IRequest extends Request {
 }
 
 export const addBook: RequestHandler = async (req: IRequest | any, res) => {
-    const book = new Book(req.body as IBook);
-    const uid = req.uid;
+  const uid = req.uid;
 
-    try {
-        if (!req.body) {
-            return res.status(400).json({
-                ok: false,
-                msg: "content POST request not provided",
-            });
-        }
+  console.log("BOOk");
 
-        book.user = uid;
-        const additionalInfo = await getISBN(book.title);
-
-        book.additionalInfo = additionalInfo;
-
-        const doc = await book.save();
-
-        return res.status(201).json({
-            ok: true,
-            msg: "Book saved",
-            doc,
-        });
-    } catch (error) {
-        console.log(error);
-        return res.json({
-            ok: false,
-            msg: message.errorDb,
-        });
+  try {
+    if (!req.body) {
+      return res.status(400).json({
+        ok: false,
+        msg: "content POST request not provided",
+      });
     }
+
+    const isDuplicated = await Book.findOne({ title: req.body.title });
+    if (isDuplicated) {
+      return res.status(400).json({
+        ok: false,
+        msg: "book alrady in DB",
+      });
+    }
+
+    const book = new Book(req.body as IBook);
+    book.user = uid;
+    const additionalInfo = await getISBN(book.title);
+
+    book.additionalInfo = additionalInfo;
+
+    const doc = await book.save();
+
+    return res.status(201).json({
+      ok: true,
+      msg: "Book saved",
+      doc,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      ok: false,
+      msg: message.errorDb,
+    });
+  }
 };
 
 export const updateBook: RequestHandler<{ id: string }> = async (req, res) => {
-    const bookId = req.params.id;
-    const { uid } = req;
+  const bookId = req.params.id;
+  const { uid } = req;
 
-    try {
-      const book = await Book.findOne({ id: bookId })!;
+  try {
+    const book = await Book.findOne({ id: bookId })!;
 
-      if (!book) {
-        return res.status(204).json({
-          ok: false,
-          msg: "Book not found",
-        });
-      }
-
-      if (book.user !== uid) {
-        return res.status(401).json({
-          ok: false,
-          msg: "User unauthorized no update",
-        });
-      }
-      // TODO :Change info wheen is update?
-      const additionalInfo = await getISBN(book.title);
-
-      const bookToUpdate = {
-        ...(req.body as IBook),
-        additionalInfo,
-      };
-
-      const updated = await Book.findByIdAndUpdate(bookId, bookToUpdate, {
-        new: true,
-        lean: true,
+    if (!book) {
+      return res.status(204).json({
+        ok: false,
+        msg: "Book not found",
       });
-
-      return res.status(201).json({
-        ok: true,
-        updated,
-      });
-    } catch (error) {
-        console.error(error);
-        return res.status(401).json({
-            ok: false,
-            msg: message.errorDb,
-        });
     }
+
+    if (book.user !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "User unauthorized no update",
+      });
+    }
+
+    const updated = await Book.findByIdAndUpdate(bookId, req.body, {
+      new: true,
+      lean: true,
+    });
+
+    return res.status(201).json({
+      ok: true,
+      updated,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      ok: false,
+      msg: message.errorDb,
+    });
+  }
 };
 
 export const deleteBook: RequestHandler<{ id: string }> = async (req, res) => {
